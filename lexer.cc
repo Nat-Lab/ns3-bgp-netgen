adbaf5331902aa7c0531072cd1085fb5bc042c33
Honghao Zeng <nat@nat.moe>
2019-02-22 18:54:31 -0500

    lexer: fix & clean

--- lexer.cc
+++ lexer.cc
@@ -1,2 +1,209 @@
 #include "lexer.h"
-#include "parser.h"
+#include <iostream>
+
+LexOut eat_garbage(std::string &in) {
+    std::regex r_space ("^ *\t*(#.*\n)? *\t*");
+    std::smatch m_space;
+
+    if (std::regex_search(in, m_space, r_space)) {
+        std::string space = m_space[0];
+        str_shift(in, space.length());
+    }
+}
+
+std::string str_shift(std::string &str, uint32_t n) {
+    auto shifted = str.substr(0, n);
+    str = str.substr(n, str.size() - n);
+    return shifted;
+}
+
+LexOut any(const Lexers lexs, std::string &in) {
+    for (auto lex : lexs) {
+        auto out = (*lex)(in);
+        if (std::get<0>(out)) return out;
+    }
+
+    return LexOut (false, NULL);
+}
+
+LexOut lex_addr(std::string &in) {
+    std::regex r_v4 ("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)");
+    std::smatch m_v4;
+
+    if (std::regex_search(in, m_v4, r_v4)) {
+        auto lex_item = new LexicalItem;
+        lex_item->type = Type::VAR;
+        lex_item->mtype = MinorType::VAR_ADDR;
+        lex_item->item = m_v4[0];
+
+        str_shift(in, lex_item->item.length());
+        return LexOut (true, lex_item);
+    } else return LexOut (false, NULL);
+}
+
+LexOut lex_prefix_len(std::string &in) {
+    std::regex r_len ("^\\/[0-9]{1,2}");
+    std::smatch m_len;
+
+    if (std::regex_search(in, m_len, r_len)) {
+        auto lex_item = new LexicalItem;
+        lex_item->type = Type::VAR;
+        lex_item->mtype = MinorType::VAR_PREFIX_LEN;
+        lex_item->item = m_len[0];
+
+        str_shift(in, lex_item->item.length());
+        return LexOut (true, lex_item);
+    } else return LexOut (false, NULL);
+}
+
+LexOut lex_name(std::string &in) {
+    std::regex r_name ("^[a-zA-Z]+[a-zA-Z0-9\\-_]*");
+    std::smatch m_name;
+
+    if (std::regex_search(in, m_name, r_name)) {
+        auto lex_item = new LexicalItem;
+        lex_item->type = Type::VAR;
+        lex_item->mtype = MinorType::VAR_NAME;
+        lex_item->item = m_name[0];
+
+        str_shift(in, lex_item->item.length());
+        return LexOut (true, lex_item);
+    } else return LexOut (false, NULL);
+}
+
+LexOut lex_asn(std::string &in) {
+    std::regex r_asn ("^[0-9]+");
+    std::smatch m_asn;
+
+    if (std::regex_search(in, m_asn, r_asn)) {
+        auto lex_item = new LexicalItem;
+        lex_item->type = Type::VAR;
+        lex_item->mtype = MinorType::VAR_ASN;
+        lex_item->item = m_asn[0];
+
+        str_shift(in, lex_item->item.length());
+        return LexOut (true, lex_item);
+    } else return LexOut (false, NULL);
+}
+
+LexOut lex_keyword(std::string &in) {
+    std::regex r_kw ("^(network|prefix|tap(_(name|mode))?|router|as|dev(ices?)?|peers?|routes?|connect|address|via|options|log)");
+    std::smatch m_kw;
+
+    if (std::regex_search(in, m_kw, r_kw)) {
+        auto lex_item = new LexicalItem;
+        lex_item->type = Type::KEYWORD;
+        lex_item->item = m_kw[0];
+        str_shift(in, lex_item->item.length());
+
+        if (lex_item->item == "network") lex_item->mtype = MinorType::KW_NETWORK;
+        if (lex_item->item == "prefix") lex_item->mtype = MinorType::KW_PREFIX;
+        if (lex_item->item == "tap") lex_item->mtype = MinorType::KW_TAP;
+        if (lex_item->item == "tap_name") lex_item->mtype = MinorType::KW_TAP_NAME;
+        if (lex_item->item == "tap_mode") lex_item->mtype = MinorType::KW_TAP_MODE;
+        if (lex_item->item == "router") lex_item->mtype = MinorType::KW_ROUTER;
+        if (lex_item->item == "as") lex_item->mtype = MinorType::KW_AS;
+        if (lex_item->item == "dev") lex_item->mtype = MinorType::KW_DEV;
+        if (lex_item->item == "device") lex_item->mtype = MinorType::KW_DEVICE;
+        if (lex_item->item == "devices") lex_item->mtype = MinorType::KW_DEVICES;
+        if (lex_item->item == "peer") lex_item->mtype = MinorType::KW_PEER;
+        if (lex_item->item == "peers") lex_item->mtype = MinorType::KW_PEERS;
+        if (lex_item->item == "route") lex_item->mtype = MinorType::KW_ROUTE;
+        if (lex_item->item == "routes") lex_item->mtype = MinorType::KW_ROUTERS;
+        if (lex_item->item == "connect") lex_item->mtype = MinorType::KW_CONNECT;
+        if (lex_item->item == "address") lex_item->mtype = MinorType::KW_ADDRESS;
+        if (lex_item->item == "via") lex_item->mtype = MinorType::KW_VIA;
+        if (lex_item->item == "options") lex_item->mtype = MinorType::KW_OPTIONS;
+        if (lex_item->item == "log") lex_item->mtype = MinorType::KW_LOG;
+
+        return LexOut (true, lex_item);
+
+    } else return LexOut (false, NULL);
+}
+
+LexOut lex_comment(std::string &in) {
+    std::regex r ("^#.*\\r?\\n");
+    std::smatch m;
+
+    if (std::regex_search(in, m, r)) {
+        auto lex_item = new LexicalItem;
+        std::string s = m[0];
+        str_shift(in, s.length());
+
+        lex_item->type = Type::IGNORE;
+        lex_item->mtype = MinorType::IGN_COMMENT;
+        lex_item->item = m[0];
+
+        return LexOut(true, lex_item);
+    } else return LexOut(false, NULL);
+}
+
+LexOut lex_token(std::string &in) {
+    std::regex r_token ("^((\\r)?\\n|;|\\{|\\})");
+    std::smatch m_token;
+
+    if (std::regex_search(in, m_token, r_token)) {
+        auto lex_item = new LexicalItem;
+        lex_item->type = Type::TOKEN;
+        lex_item->item = m_token[0];
+        str_shift(in, lex_item->item.length());
+
+        if (lex_item->item == "\r\n" || lex_item->item == "\n") {
+            lex_item->type = Type::IGNORE;
+            lex_item->mtype = MinorType::IGN_NEWLINE;
+        }
+        if (lex_item->item == "{") lex_item->mtype = MinorType::TKN_LBRACE;
+        if (lex_item->item == "}") lex_item->mtype = MinorType::TKN_RBRACE;
+        if (lex_item->item == ";") lex_item->mtype = MinorType::TKN_SEMICOL;
+
+        return LexOut (true, lex_item);
+
+    } else return LexOut (false, NULL);
+}
+
+LexOut lex_bool(std::string &in) {
+    std::regex r_token ("^(on|off|true|false)");
+    std::smatch m_token;
+
+    if (std::regex_search(in, m_token, r_token)) {
+        auto lex_item = new LexicalItem;
+        lex_item->type = Type::BOOL;
+        lex_item->item = m_token[0];
+        str_shift(in, lex_item->item.length());
+
+        if (lex_item->item == "true" || lex_item->item == "on") 
+            lex_item->mtype = MinorType::BOOL_TRUE;
+        if (lex_item->item == "false" || lex_item->item == "off") 
+            lex_item->mtype = MinorType::BOOL_FALSE;
+
+        return LexOut (true, lex_item);
+
+    } else return LexOut (false, NULL);
+}
+
+bool lexer(std::string &in, LexicalItems &out) {
+    Lexers lexs;
+    lexs.push_back(&lex_comment);
+    lexs.push_back(&lex_keyword);
+    lexs.push_back(&lex_token);
+    lexs.push_back(&lex_bool);
+    lexs.push_back(&lex_name);
+    lexs.push_back(&lex_addr);
+    lexs.push_back(&lex_prefix_len);
+    lexs.push_back(&lex_asn);
+
+    while (in.length() > 0) {
+        eat_garbage(in);
+        auto r = any(lexs, in);
+        if(!std::get<0> (r)) {
+            std::cerr << "lexer: stopped at: " << in.substr(0, 100) << "..." << std::endl;
+            return false;
+        } else {
+            auto item = std::get<1>(r);
+            if (item->type != Type::IGNORE) out.push_back(*item);
+        }
+    }
+
+
+    return true;
+}
--- lexer.h
+++ lexer.h
@@ -4,55 +4,74 @@
 #include <vector>
 #include <string>
 #include <tuple>
+#include <regex>
 #include <unistd.h>
 
 typedef enum Type {
-	TOKEN,
-	KEYWORD,
-	VAR
+    TOKEN,
+    KEYWORD,
+    VAR,
+    BOOL,
+    IGNORE
 } Type;
 
 typedef enum MinorType {
-	VAR_ADDR,
-	VAR_PREFIX_LEN,
-	VAR_NAME,
-	VAR_ASN,
-	KW_DEVICES,
-	KW_DEVICE,
-	KW_DEV,
-	KW_NET,
-	KW_ROUTER,
-	KW_ROUTES,
-	KW_ROUTE,
-	KW_ASN,
-	KW_VIA,
-	KW_ADDR,
-	KW_CONNECT,
-	KW_PEERS,
-	KW_PEER,
-	TKN_LBRACE,
-	TKN_RBRACE,
-	TKN_SEMICOL
+    VAR_ADDR,
+    VAR_PREFIX_LEN,
+    VAR_NAME,
+    VAR_ASN,
+    VAR_BOOL,
+    TKN_LBRACE,
+    TKN_RBRACE,
+    TKN_SEMICOL,
+    IGN_NEWLINE,
+    IGN_COMMENT,
+    KW_NETWORK,
+    KW_PREFIX,
+    KW_TAP,
+    KW_TAP_NAME,
+    KW_TAP_MODE,
+    KW_ROUTER,
+    KW_AS,
+    KW_DEV,
+    KW_DEVICE,
+    KW_DEVICES,
+    KW_PEER,
+    KW_PEERS,
+    KW_ROUTE,
+    KW_ROUTERS,
+    KW_CONNECT,
+    KW_ADDRESS,
+    KW_VIA,
+    KW_OPTIONS,
+    KW_LOG,
+    BOOL_TRUE,
+    BOOL_FALSE
 } MinorType;
 
 typedef struct LexicalItem {
-	Type type;
-	MinorType mtype;
-	std::string element;
+    Type type;
+    MinorType mtype;
+    std::string item;
 } LexicalItem;
 
 typedef std::vector<LexicalItem> LexicalItems;
-typedef std::tuple<bool, LexicalItem, std::string> LexOut;
-typedef LexOut (*Lexer) (std::string in);
+typedef std::tuple<bool, LexicalItem*> LexOut;
+typedef LexOut (*Lexer) (std::string &in);
 typedef std::vector<Lexer> Lexers;
 
-LexOut any(Lexers lexs);
-LexOut str_match(std::string in, std::string match);
-LexOut lex_addr(std::string in);
-LexOut lex_prefix_len(std::string in);
-LexOut lex_name(std::string in);
-LexOut lex_asn(std::string in);
-LexOut lex_keyword(std::string in);
-LexOut lex_token(std::string in);
+void eat_space(std::string &in);
+std::string str_shift(std::string &in, uint32_t n);
+
+LexOut any(const Lexers lexs, std::string &in);
+LexOut lex_addr(std::string &in);
+LexOut lex_prefix_len(std::string &in);
+LexOut lex_name(std::string &in);
+LexOut lex_asn(std::string &in);
+LexOut lex_keyword(std::string &in);
+LexOut lex_token(std::string &in);
+LexOut lex_bool(std::string &in);
+
+bool lexer(std::string &in, LexicalItems &out);
 
 #endif // BGP_CONF_L_H_
